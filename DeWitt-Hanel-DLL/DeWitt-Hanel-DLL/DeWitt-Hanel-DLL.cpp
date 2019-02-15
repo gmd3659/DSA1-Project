c54ddc5e09430586e506b22adcfd55df914070cd
gmd3659 <gmd3659@IGM2000-20>
2019-02-15 17:25:23 -0500

    Last 4 methods done I think?

--- DeWitt-Hanel-DLL/DeWitt-Hanel-DLL/DeWitt-Hanel-DLL.cpp
+++ DeWitt-Hanel-DLL/DeWitt-Hanel-DLL/DeWitt-Hanel-DLL.cpp
@@ -7,6 +7,11 @@ int** data;
 int width;
 int height;
 
+int xStart;
+int yStart;
+int xEnd;
+int yEnd;
+
 
 void test()
 {
@@ -33,4 +38,44 @@ void SetMaze(const int** p_data, int p_width, int p_height)
 int** GetMaze()
 {
 
+}
+
+void SetStart(int xPos, int yPos)
+{
+	xStart = xPos;
+	yStart = yPos;
+}
+
+void GetStart(int& xPos, int& yPos)
+{
+	if (xStart > 0 && yStart > 0)
+	{
+		xPos = xStart;
+		yPos = yStart;
+	}
+	else
+	{
+		xPos = -1;
+		yPos = -1;
+	}
+}
+
+void SetEnd(int xPos, int yPos)
+{
+	xEnd = xPos;
+	yEnd = yPos;
+}
+
+void GetEnd(int& xPos, int& yPos)
+{
+	if (xEnd > 0 && yEnd > 0)
+	{
+		xPos = xEnd;
+		yPos = yEnd;
+	}
+	else
+	{
+		xPos = -1;
+		yPos = -1;
+	}
 }
