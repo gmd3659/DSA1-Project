37aa24aaf45effb534b343c1f80f6dd67b2ec961
bxh9261 <bxh9261@g.rit.edu>
2019-02-15 17:20:45 -0500

    2 methods please work

--- DeWitt-Hanel-DLL/DeWitt-Hanel-DLL/DeWitt-Hanel-DLL.cpp
+++ DeWitt-Hanel-DLL/DeWitt-Hanel-DLL/DeWitt-Hanel-DLL.cpp
@@ -3,4 +3,34 @@
 
 #include "stdafx.h"
 
+int** data;
+int width;
+int height;
 
+
+void test()
+{
+
+}
+
+char* GetTeam()
+{
+	char team[] = "Group 10 -- Gavin Dewitt and Brad Hanel";
+	return team;
+}
+
+void SetMaze(const int** p_data, int p_width, int p_height)
+{
+	for (int i = 0; sizeof p_data / sizeof p_data[0]; i++)
+	{
+		for (int j = 0; sizeof p_data[0] / sizeof(int); j++)
+		{
+			*data[i, j] = *p_data[i, j];
+		}
+	}
+}
+
+int** GetMaze()
+{
+
+}
