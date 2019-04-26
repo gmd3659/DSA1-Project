5d9e30ae06f0f2542f5d72c303516394fc66911c
bxh9261 <bxh9261@g.rit.edu>
2019-04-26 11:20:40 -0400

    milestone 3 started

--- DeWitt-Hanel-DLL/DeWitt-Hanel-DLL/DeWitt-Hanel-DLL.cpp
+++ DeWitt-Hanel-DLL/DeWitt-Hanel-DLL/DeWitt-Hanel-DLL.cpp
@@ -17,15 +17,15 @@ size_t gnpCall = 0;
 
 char team[40] = "Group 10 -- Gavin Dewitt and Brad Hanel";
 
-__declspec(dllexport) char* GetTeam();
-__declspec(dllexport) bool SetMaze(const int** p_data, int p_width, int p_height);
-__declspec(dllexport) int** GetMaze(int& p_width, int& p_height);
-__declspec(dllexport) bool GetNextPosition(int& xpos, int& ypos);
-__declspec(dllexport) bool SetStart(int xPos, int yPos);
-__declspec(dllexport) bool GetStart(int& xPos, int& yPos);
-__declspec(dllexport) bool SetEnd(int xPos, int yPos);
-__declspec(dllexport) bool GetEnd(int& xPos, int& yPos);
-__declspec(dllexport) bool Restart();
+extern "C" __declspec(dllexport) char* GetTeam();
+extern "C" __declspec(dllexport) bool SetMaze(const int** p_data, int p_width, int p_height);
+extern "C" __declspec(dllexport) int** GetMaze(int& p_width, int& p_height);
+extern "C" __declspec(dllexport) bool GetNextPosition(int& xpos, int& ypos);
+extern "C" __declspec(dllexport) bool SetStart(int xPos, int yPos);
+extern "C" __declspec(dllexport) bool GetStart(int& xPos, int& yPos);
+extern "C" __declspec(dllexport) bool SetEnd(int xPos, int yPos);
+extern "C" __declspec(dllexport) bool GetEnd(int& xPos, int& yPos);
+extern "C" __declspec(dllexport) bool Restart();
 
 
 // Returns a string that has both team members name.  Have the C string value return both team member names.  There is no defined format for this.
@@ -58,7 +58,7 @@ bool SetMaze(const int** p_data, int p_width, int p_height)
 	{
 		for (int j = 0; j < g.width; j++)
 		{
-			if (g.data[i][j] == 0)
+			if (g.data[i][j] > 0)
 			{
 				g.openList.push_back(Vertex(i, j, g.xEnd, g.yEnd));
 			}
@@ -129,10 +129,11 @@ bool GetNextPosition(int& xpos, int& ypos)
 	//Check for more than 0 valid adjacent spaces
 	if (tempVector.size() == 1)
 	{
-		//If less than 0 valid spaces
+		//If 0 valid spaces
 
 		//Pop the current vertex off the stack
 		g.previousPath.pop();
+		
 
 		//Set lastVert equal to the previous vertex to step backwards
 		Vertex lastVert = g.previousPath.top();
@@ -153,7 +154,7 @@ bool GetNextPosition(int& xpos, int& ypos)
 
 		for (size_t i = 1; i < tempVector.size(); i++)
 		{
-			if (tempVector[i].getHeur() < lowest.getHeur())
+			if (tempVector[i].getHeur() > lowest.getHeur())
 			{
 				lowest = tempVector[i];
 			}
